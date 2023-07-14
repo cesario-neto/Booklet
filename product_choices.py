099061624c9dbe3dcd609ee49e20ffd9be02c068
cesario_neto <cesarioneto7@gmail.com>
2023-07-14 16:43:22 -0300

    Form field choices

--- product_choices.py
+++ product_choices.py
@@ -1,4 +1,7 @@
 # Products that will be used
 # Place tuples with 2 objects for the text
-name_choice = [('produto1', 'produto1'), ('produto2',
-                                          'produto2'), ('produto3', 'produto3')]
+name_choice = [('produto1', 'Produto1'), ('produto2',
+                                          'Produto2'), ('produto3', 'Produto3')]
+
+type_of_sale = [('espécie', 'Espécie'), ('pix', 'Pix'), ('cartao_debito',
+                                                         'Cartão Débito'), ('cartao_credito', 'Cartão Crédito')]
