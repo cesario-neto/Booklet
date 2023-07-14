1f7e586b57ff4e4500f89bc5d0ccbc4689d7855b
cesario_neto <cesarioneto7@gmail.com>
2023-07-14 16:47:28 -0300

    Creating route to save order

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from booklet import Booklet
+from forms import ProductForm
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+csrf = CSRFProtect(app)
+booklet = Booklet()
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def booklet_view():
+    form = ProductForm()
+    return render_template('booklet.html', form=form)
+
+
+@app.route('/booklet/create_order', methods=['POST'])
+def create_order():
+    form = ProductForm(request.form)
+
+    if form.validate_on_submit():
+        name = form.name.data
+        price = form.price.data
+        quantity = form.quantity.data
+        type_of_sale = form.type_of_sale.data
+
+        dictionary = {
+            'product': name,
+            'price': price,
+            'quantity': quantity,
+            'type_of_sale': type_of_sale
+        }
+
+        booklet.add_order(dictionary)
+
+    return redirect(url_for('booklet_view'))
 
 
 if __name__ == '__main__':
